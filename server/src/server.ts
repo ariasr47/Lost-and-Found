605a32f3b7423f432b460d536f1766535860ad0f
Rodrigo Arias <rodrigoarias@Rodrigos-MacBook-Pro-2.local>
2021-12-05 21:17:58 -0800

    added secure cookie

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,6 +18,7 @@ const SESSION_OPTIONS: SessionOptions = {
     maxAge: 24 * 60 * 60 * 1000,
     signed: true,
     secure: true,
+    httpOnly: false,
   },
   name: "ucdavis-lostandfound-session-cookie",
   resave: false,
@@ -38,15 +39,13 @@ morgan.token("id", (req, res, arg) => {
   return req["id"].split("-")[0];
 });
 
-app.set("trust proxy", 1);
-
 app.use(addRequestId);
 app.use(
   morgan("[:date #:id] :method :url", {
     immediate: true,
   })
 );
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(config.SESSION.SECRET));
@@ -58,20 +57,8 @@ app.use(passport.session());
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
 
-console.log(process.env);
-console.log(`process.env.NODE_ENV = ${process.env.NODE_ENV}`);
-console.log(`__dirname = ${__dirname}`);
-
-app.use((req, res, next) => {
-  console.log(`req.path = ${req.path}`);
-  console.log(`headers =`);
-  console.log(req.headers);
-  console.log(req.complete);
-  console.log(req.originalUrl);
-});
-
 if (process.env.NODE_ENV === "production") {
-  app.use(helmet.hidePoweredBy);
+  app.use(helmet.hidePoweredBy());
 
   app.use(
     "/static/js",
@@ -104,22 +91,19 @@ if (process.env.NODE_ENV === "production") {
 
   app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
-  app.get("/*", async (req: Request, res: Response) => {
-    console.log('app.get("/*")');
-    try {
-      res.sendFile(path.join(__dirname, "./index.html"));
-    } catch (error) {
-      console.error(error);
-    }
+  app.get("/*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "./index.html"));
   });
 }
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.info(`Server started on port: ${PORT}`);
-  try {
-    const Sequelize = await sequelize.sync();
-    console.info("Connection to database is established!");
-  } catch (error) {
-    console.error(error);
-  }
+  sequelize
+    .sync()
+    .then((Sequelize) => {
+      console.info("Connection to database is established!");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
